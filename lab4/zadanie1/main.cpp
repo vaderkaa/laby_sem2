3f3b04aadd79a10ec4cf07997fbe07059d16539a
Jadwiga Myna <01208385@pw.edu.pl>
2026-04-26 14:17:47 +0200

    Dodano inteligentne wskazniki do main

--- lab4/zadanie1/main.cpp
+++ lab4/zadanie1/main.cpp
@@ -76,7 +76,6 @@ int main() {
 
     std::cout << "\n*************** PART 4 (Smart Pointers) ***************\n\n";
 
-    /*
     struct Dummy {
         char id;
         Dummy(char id) : id(id) {
@@ -87,33 +86,57 @@ int main() {
         }
     };
 
-    // [TODO 1]: Zdefiniuj dwa puste wektory przechowujÄce shared_ptr do Dummy
-    // v1
-    // v2
+    //INTELIGENTNE WSKAZNIKI - zamiast pilnowac pamieci samodzielnie, obiekty robia to za nas - nie ma wyciekow pamieci lub podwojnych zwolnien
+    //smart pointers to opakowania na surowe wskazniki - jesli opakowanie umiera, to sprzata swoja zawartosc
+
+    //std::shared_ptr - kilka czesci programu ma dostep do tego samego obiektu i zadna nie jest ta "glowna"
+    // [TODO 1]: Zdefiniuj dwa puste wektory przechowujace shared_ptr do Dummy
+    //przed klamerka beda zyly dluzej niz w klamerce
+    std::vector<std::shared_ptr<Dummy>> v1;
+    std::vector<std::shared_ptr<Dummy>> v2;
+
     {
-        // [TODO 1]: Zdefiniuj 3 zmienne przechowujÄce shared_ptr do Dummy z identyfikatorami A, B i C
-        // A
-        // B
-        // C
+        // [TODO 1]: Zdefiniuj 3 zmienne przechowujace shared_ptr do Dummy z identyfikatorami A, B i C
+        std::shared_ptr<Dummy> A = std::make_shared<Dummy>('A'); //prawa: wskaznik o nazwie A, ktory bedzie patrzyl na obiekt typu Dummy
+        //lewa: rezerwacja miejsca na obiekt typu Dummy o literce A i licznik odwolan do niego
+        //nastepuje rezerwacja pamieci na literke A, wyswietla A created, licznik odwolan zotaje ustawiony na 1
+        std::shared_ptr<Dummy> B = std::make_shared<Dummy>('B'); //powstaje obiekt B, licznik = 1
+        std::shared_ptr<Dummy> C = std::make_shared<Dummy>('C'); //powstaje obiekt C, licznik = 1
 
         // [TODO 1]: Dodaj A, B i C do v1
+        //push_back zadziala, bo shared_ptr kocha byc kopiowany
+        v1.push_back(A); //licznik dla A to 2, bo A jest trzymany przez zmienna lokalna A i wektor v1
+        v1.push_back(B); //licznik dla B to 2
+        v1.push_back(C); //licznik dla C to 2
 
         // [TODO 1]: Dodaj B i C do v2
+        v2.push_back(B); //licznik dla B to 3
+        v2.push_back(C); //licznik dla C to 3
     }
+    //zmienne lokalne A, B, C gina, liczniki to teraz 1, 2, 2, ale reszta obiektow nie umiera
 
     std::cout << "\nClearing v1" << std::endl;
-    v1.clear();
+    v1.clear(); //A spada na 0 i ginie, B, C spadaja na 1 i gina
+    //bez klamerek A spada na 1, B, C spadaja na 2
     std::cout << std::endl;
 
     std::cout << "Clearing v2" << std::endl;
-    v2.clear();
+    v2.clear(); //B, C spadaja na 0 i gina
+    //bez klamerek B, C spadaja na 1 i nie gina
     std::cout << std::endl;
 
-    // [TODO 2]: Zdefiniuj pusty wektor przechowujÄcy unique_ptr do Dummy
-    // v3
+    //std::unique_ptr - tylko jeden wskaznik moze posiadac dany obiekt w danej chwili, unique_ptr jest niekopiowany, jest tylko jeden!!!
+    // [TODO 2]: Zdefiniuj pusty wektor przechowujacy unique_ptr do Dummy
+    std::vector<std::unique_ptr<Dummy>> v3; //dynamiczna tablica, ktora bedzie przechowywac wskazniki na obiekty typu Dummy
+    //jesli usune wektor lub jego element, to wskazniki automatycznie wywolaja destruktory obiektow typu Dummy
 
     // [TODO 2]: Dodaj do v3 dwa unique_ptr z identyfikatorami D i E
-    */
+    v3.emplace_back(std::make_unique<Dummy>('D'));
+    //emplace_back - sprytniejsze push_back, bo zamiast tworzyc wskaznik i go kopiowac, emplace_back tworzy go bezposrednio wewnatrz wektora (bez tego trzebaby jakos std::move)
+    //std::make_unique - tworzenie obiektu typu Dummy z literka D
+    v3.emplace_back(std::make_unique<Dummy>('E'));
+
+    std::cout << std::endl;
 
     return 0;
 }
